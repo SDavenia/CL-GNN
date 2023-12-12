76731474842fe78d057c0fb14b7ba45652a807af
SDavenia <davesam00@gmail.com>
2023-12-12 19:12:18 +0100

    checkpoint

--- training.py
+++ training.py
@@ -14,32 +14,19 @@ def epoch_time(start_time, end_time):
 
 
 def train(model, iterator, optimizer, criterion):
-    # We will calculate loss and accuracy epoch-wise based on average batch accuracy
-    epoch_loss = 0
 
-    # You always need to set your model to training mode
-    # If you don't set your model to training mode the error won't propagate back to the weights
+    epoch_loss = 0
     model.train()
 
-    # We calculate the error on batches so the iterator will return matrices with shape [BATCH_SIZE, VOCAB_SIZE]
     for batch in iterator:
-        # We reset the gradients from the last step, so the loss will be calculated correctly (and not added together)
         optimizer.zero_grad()
-
-        # This runs the forward function on your model (you don't need to call it directly)
         predictions = model(batch.x_1.float(), batch.edge_index_1, batch.x_1_batch, 
                             batch.x_2.float(), batch.edge_index_2, batch.x_2_batch)
 
-        # Calculate the loss on the output of the model
         loss = criterion(predictions, batch.distance)
-
-        # Propagate the error back on the model (this means changing the initial weights in your model)
-        # Calculate gradients on parameters that requries grad
         loss.backward()
-        # Update the parameters
         optimizer.step()
 
-        # We add batch-wise loss to the epoch-wise loss
         epoch_loss += loss.item()
     return (epoch_loss / len(iterator))
 
@@ -47,15 +34,9 @@ def train(model, iterator, optimizer, criterion):
 def evaluate(model, iterator, criterion):
 
     epoch_loss = 0
-
-    # On the validation dataset we don't want training so we need to set the model on evaluation mode
     model.eval()
 
-    # Also tell Pytorch to not propagate any error backwards in the model or calculate gradients
-    # This is needed when you only want to make predictions and use your model in inference mode!
     with torch.no_grad():
-
-        # The remaining part is the same with the difference of not using the optimizer to backpropagation
         for batch in iterator:
             predictions = model(batch.x_1.float(), batch.edge_index_1, batch.x_1_batch, 
                                 batch.x_2.float(), batch.edge_index_2, batch.x_2_batch)
@@ -63,7 +44,6 @@ def evaluate(model, iterator, criterion):
 
             epoch_loss += loss.item()
 
-    # Return averaged loss on the whole epoch!
     return (epoch_loss / len(iterator))
 
 
@@ -79,18 +59,15 @@ def training_loop(model, train_iterator, optimizer, criterion, valid_iterator, e
     train_losses = []
     validation_losses = []
 
-    # We loop forward on the epoch number
     start_time = time.time()
     for epoch in range(N_EPOCHS):
-        # Train the model on the training set using the dataloader
         train_loss = train(model, train_iterator, optimizer, criterion)
-        # And validate your model on the validation set
         valid_loss = evaluate(model, valid_iterator, criterion)
 
         train_losses.append(train_loss)
         validation_losses.append(valid_loss)
 
-        # If we find a better model, we save the weights so later we may want to reload it
+        # Save model with best validation loss
         if valid_loss < best_valid_loss:
             best_valid_loss = valid_loss
             model.save() # Save model in models folder
